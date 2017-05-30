5b2486a117cfea3c703f645cbbe4e77f9f2c4729
duyetMTA <minhduyet96@gmail.com>
2017-05-30 11:52:07 +0700

    BillDAO,Billinfo,DangNhap

---
+++ BillDAO.cs
@@ -0,0 +1,43 @@
+﻿using QuanLySieuThi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySieuThi.DAO
+{
+   public class BillDAO
+    {
+        private static BillDAO instance;
+
+        public static BillDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BillDAO();
+                return BillDAO.instance;
+            }
+            private set
+            {
+                BillDAO.instance = value;
+            }
+
+        }
+        private BillDAO() { }
+        public List<Bill> getListBill()
+        {
+            List<Bill> list = new List<Bill>();
+
+            DataTable data = DataProvider.Instance.ExecuteQuery("Select * from dbo.Bill");
+            foreach (DataRow item in data.Rows)
+            {
+                Bill bill = new Bill(item);
+                list.Add(bill);
+            }
+            return list;
+        }
+    }
+}
---
+++ BillInfoDAO.cs
@@ -0,0 +1,50 @@
+﻿using QuanLySieuThi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySieuThi.DAO
+{
+    public class BillInfoDAO
+    {
+        private static BillInfoDAO instance;
+
+        public static BillInfoDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BillInfoDAO();
+                return BillInfoDAO.instance;
+            }
+            private set
+            {
+                BillInfoDAO.instance = value;
+            }
+           
+        }
+        private BillInfoDAO() { }
+
+        internal List<BillInfo> getListBillInfo(List<Bill> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BillInfo> getListBillInfo(int id)
+        {
+            List<BillInfo> list = new List<BillInfo>();
+           
+            DataTable data = DataProvider.Instance.ExecuteQuery("Select * from dbo.BillInfo where idBill =" + id);
+            foreach (DataRow item in data.Rows)
+            {
+                BillInfo billInfo = new BillInfo(item);
+                list.Add(billInfo);
+            }
+            return list;
+        }
+
+    }
+}
